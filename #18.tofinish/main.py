489d4c523bfb5f3d306b8ecb1465c0639fb74742
Thuwa77 <guillaume.szwankowski@epitech.eu>
2019-07-02 15:58:25 +0900

    #18 first try

---
+++ #18.tofinish/main.py
@@ -0,0 +1,57 @@
+"""
+Given an array of integers and a number k, where 1 <= k <= length of the array, compute the maximum values of each subarray of length k.
+
+For example, given array = [10, 5, 2, 7, 8, 7] and k = 3, we should get: [10, 7, 8, 8], since:
+
+    10 = max(10, 5, 2)
+    7 = max(5, 2, 7)
+    8 = max(2, 7, 8)
+    8 = max(7, 8, 7)
+
+Do this in O(n) time and O(k) space. You can modify the input array in-place and you do not need to store the results. You can simply print them out as you compute them.
+"""
+
+from typing import List
+
+
+# with n = len(numbers), time O(n * k), space O(n)
+def bruteForce(numbers: List[int], k: int) -> List[int]:
+	maxSubarrays = []
+
+	for i in range(0, len(numbers) + 1 - k):
+		maxSubarrays.append(max(numbers[i:i + k]))
+
+	return maxSubarrays
+
+
+def maxSubarrays(numbers: List[int], k: int) -> List[int]:
+	if k < 1 or k > len(numbers):
+		return None
+
+	return bruteForce(numbers, k)
+
+
+print(
+    f"maxSubarrays([10, 5, 2, 7, 8, 7], 0) = {maxSubarrays([10, 5, 2, 7, 8, 7], 0)}"
+)
+print(
+    f"maxSubarrays([10, 5, 2, 7, 8, 7], 1) = {maxSubarrays([10, 5, 2, 7, 8, 7], 1)}"
+)
+print(
+    f"maxSubarrays([10, 5, 2, 7, 8, 7], 2) = {maxSubarrays([10, 5, 2, 7, 8, 7], 2)}"
+)
+print(
+    f"maxSubarrays([10, 5, 2, 7, 8, 7], 3) = {maxSubarrays([10, 5, 2, 7, 8, 7], 3)}"
+)
+print(
+    f"maxSubarrays([10, 5, 2, 7, 8, 7], 4) = {maxSubarrays([10, 5, 2, 7, 8, 7], 4)}"
+)
+print(
+    f"maxSubarrays([10, 5, 2, 7, 8, 7], 5) = {maxSubarrays([10, 5, 2, 7, 8, 7], 5)}"
+)
+print(
+    f"maxSubarrays([10, 5, 2, 7, 8, 7], 6) = {maxSubarrays([10, 5, 2, 7, 8, 7], 6)}"
+)
+print(
+    f"maxSubarrays([10, 5, 2, 7, 8, 7], 7) = {maxSubarrays([10, 5, 2, 7, 8, 7], 7)}"
+)
