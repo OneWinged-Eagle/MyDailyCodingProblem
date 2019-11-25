4f5b5be40ee31c728762a1b0dfb887416488a7d7
OneWinged-Eagle <guillaume.szwankowski@epitech.eu>
2019-11-25 18:53:27 +0900

    #164

---
+++ #164/main.go
@@ -0,0 +1,29 @@
+package main
+
+/*
+You are given an array of length n + 1 whose elements belong to the set {1, 2, ..., n}. By the pigeonhole principle, there must be a duplicate. Find it in linear time and space.
+*/
+
+import "fmt"
+
+func findDuplicate(elements []interface{}) (index int, duplicate interface{}) {
+	if len(elements) == 0 {
+		return -1, nil
+	}
+
+	set := make(map[interface{}]struct{})
+	for i, elem := range elements {
+		if _, exists := set[elem]; exists {
+			return i, elem
+		}
+
+		set[elem] = struct{}{}
+	}
+
+	return -1, nil
+}
+
+func main() {
+	i, dup := findDuplicate([]interface{} {1, 2, 3, 4, 1})
+	fmt.Printf("findDuplicate([1, 2, 3, 4, 1]) = (%d, %d)", i, dup)
+}
