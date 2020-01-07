8d352c7d6aae709c4c03f20d45fb18a96a32b157
OneWinged-Eagle <guillaume.szwankowski@epitech.eu>
2020-01-07 12:51:32 +0900

    #97

--- Medium/#97/main.go
+++ Medium/#97/main.go
@@ -26,8 +26,97 @@ d.set(1, 2, 0) # set key 1 to value 2 at time 0
 d.get(1, 0) # get key 1 at time 0 should be 2
 */
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
+
+func floorSearch(array []int, elem int) int {
+	if len(array) == 0 {
+		return -1
+	}
+
+	tmp := array
+	for half := len(tmp) / 2; half > 0; half = len(tmp) / 2 {
+		if elem == tmp[half] {
+			return tmp[half]
+		} else if elem < tmp[half] {
+			tmp = tmp[:half]
+		} else {
+			tmp = tmp[half:]
+		}
+	}
+
+	if len(tmp) == 0 || tmp[0] > elem {
+		return -1
+	}
+
+	return tmp[0]
+}
+
+type flooredMap struct {
+	m    map[int]int
+	keys []int
+}
+
+func newFlooredMap(key, value int) *flooredMap {
+	return &flooredMap{map[int]int{key: value}, []int{key}}
+}
+
+func (fm *flooredMap) set(key, value int) {
+	i := sort.SearchInts(fm.keys, key)
+
+	if i == len(fm.keys) || fm.keys[i] != key {
+		fm.keys = append(fm.keys, 0)
+		copy(fm.keys[i+1:], fm.keys[i:])
+		fm.keys[i] = key
+	}
+	fm.m[key] = value
+}
+
+func (fm flooredMap) get(key int) int {
+	k := floorSearch(fm.keys, key)
+
+	if k == -1 {
+		return -1
+	}
+
+	return fm.m[k]
+}
+
+type dateMap struct {
+	m map[int]*flooredMap
+}
+
+func (dm *dateMap) set(key, value, time int) {
+	if fm, ok := dm.m[key]; ok {
+		fm.set(time, value)
+	} else {
+		dm.m[key] = newFlooredMap(time, value)
+	}
+}
+
+func (dm *dateMap) get(key, time int) int {
+	if fm, ok := dm.m[key]; ok {
+		return fm.get(time)
+	}
+
+	return -1
+}
 
 func main() {
-	fmt.Print("TODO")
+	dm := &dateMap{make(map[int]*flooredMap)}
+
+	dm.set(1, 1, 1)
+	dm.set(1, 2, 3)
+	fmt.Printf("dm.get(1, 1) = %d (should be 1)\n", dm.get(1, 1))
+	fmt.Printf("dm.get(1, 4) = %d (should be 2)\n", dm.get(1, 4))
+
+	dm.set(1, 1, 5)
+	fmt.Printf("dm.get(1, 0) = %d (should be -1)\n", dm.get(1, 0))
+	fmt.Printf("dm.get(1, 10) = %d (should be 1)\n", dm.get(1, 10))
+
+	dm.set(1, 1, 0)
+	dm.set(1, 2, 0)
+	fmt.Printf("dm.get(1, 0) = %d (should be 2)\n", dm.get(1, 0))
 }
